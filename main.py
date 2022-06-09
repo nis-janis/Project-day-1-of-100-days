a270dbbd7430391c80fcf52fd128287a25ffb0a2
janane nisrine <83500058+nis-janis@users.noreply.github.com>
2022-06-09 17:39:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Create a greeting for your program
+print("Greeting and Hello to band name generator game")
+
+# Ask the user for the city that they grew up in
+city = input("which city did you grew up in?\n")
+
+# Ask the user for the name of a pet
+pet = input("what is your pet name?\n")
+
+# Combine the name of their city and pet and show them their band name using String concatenation
+print("Your band name could be: " + pet + " " + city)
+
